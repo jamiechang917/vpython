4a50aaad43eec0a75d00c1f5e5a415bb00cd3bbe
Jamie Chang <jamiechang917@gmail.com>
2019-09-14 19:43:51 +0800

    Add files via upload

--- extraCredit.py
+++ extraCredit.py
@@ -1,45 +1,71 @@
 #!/usr/bin/python3
-from vpython import *
 
-size = 1 #ballSize
-m = 1 #kg
-v = 100 #m/s
-h = 100 #m
+# Free Fall with Air Resistance by Jamie Chang
 
-L = 100
-b = 1 #drag
-g = 9.8 #m/s^2
+import matplotlib.pyplot as plt
+
+
+
+#Variables
+init_velocity = 100 #(m/s)
+v = init_velocity
+m = 1 #(kg)
+g = 9.8 #(m/s^2)
+b = 1 #(kg/s)
 t = 0
-dt = 0.01
+duration = 50 #(s)
+x_axis_range=(-3,duration)
+y_axis_range=(-40,100)
 
-scene = canvas(title="ExtraCredit",width=1200,height=800,x=0,y=0,center=vector(0,h/2,0),background=vector(0,0.6,0.6))
 
-floor = box(pos=vector(0,-size,0),length=L,width=30,height=0.01,texture=textures.wood)
 
-gd1 = graph(title="x-t plot",width=800,height=600,x=0,y=800,xtitle="t(s)",ytitle="x(m)")
-gd2 = graph(title="h-t plot",width=800,height=600,x=0,y=1400,xtitle="t(s)",ytitle="h(m)")
-gd3 = graph(title="v-t plot",width=800,height=600,x=0,y=2000,xtitle="t(s)",ytitle="v(m/s)")
-
-xt = gcurve(graph=gd1,color=color.red)
-ht = gcurve(graph=gd2,color=color.red)
-vt = gcurve(graph=gd3,color=color.red)
 
 #Main
-ball = sphere(radius=size,pos=vector(-L/2,h,0),texture=textures.metal,make_trail=True)
-ball.v = vector(v,0,0)
-ball.a = vector(0,-g,0)
+################################
+v_list = [v]
+t_list = [0]
 
-while ball.pos.y >= 0:
-    rate(100)
-    f = ball.v*(-b)
-    ball.a = vector(0,-g,0) + f/m
-    ball.v += (ball.a)*dt
-    ball.pos += (ball.v)*dt
-    ball_v = (ball.v.x**2+ball.v.y**2)**0.5
-    ball_x = ball.pos.x + (L/2)
-    xt.plot(pos=(t,ball_x))
-    ht.plot(pos=(t,ball.pos.y))
-    vt.plot(pos=(t,ball_v))
-    t += dt
+fig,ax = plt.subplots()
 
-print("finish")
+def setup():
+    global t,v,v_list,t_list
+    t = 0
+    v = init_velocity
+    v_list = [v]
+    t_list = [0]
+
+def draw(dt):
+    setup()
+    global t,v
+    for i in range(int(duration/dt)):
+        t += dt
+        f = -b*v
+        a = g + (f/m) #downward
+        v += (a*dt)
+        v_list.append(v)
+        t_list.append(t)
+    return v_list,t_list
+
+ax.set_xlim((x_axis_range))
+ax.set_ylim(y_axis_range)
+
+draw(dt=0.01)
+l1, = ax.plot(t_list,v_list,c="green",lw=0.8,label="dt = 0.01")
+
+draw(dt=0.1)
+l2, = ax.plot(t_list,v_list,c="#FFD800",lw=0.8,label="dt = 0.1")
+
+draw(dt=0.5)
+l3, = ax.plot(t_list,v_list,c="orange",lw=0.8,label="dt = 0.5")
+
+draw(dt=1.5)
+l4, = ax.plot(t_list,v_list,c="red",lw=0.8,label="dt = 1.5")
+
+ax.set_facecolor("#CCFFFE")
+ax.legend(handles=[l1,l2,l3,l4],loc="best")
+ax.set_xlabel(r'$time\ (s)$')
+ax.set_ylabel(r'$velocity\ (m/s , downward)$')
+ax.set_title(r'$Free\ Fall\ with\ Air\ Resistance\ by\ Jamie\ Chang$')
+
+
+plt.show()
